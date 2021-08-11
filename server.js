8538406dae42441af66fb57e4889f01b8179a396
Andres Mejia <andresdmejiaf@gmail.com>
2021-08-11 09:14:57 -0500

    agregando funcionalidad

--- server.js
+++ server.js
@@ -2,17 +2,64 @@ console.clear();
 
 const { Server } = require("net");
 
-const server = new Server();
+const host = "0.0.0.0";
 
-server.on("connection", (socket) => {
-	const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
-	console.log(`New connection from ${remoteSocket}`);
-	socket.setEncoding("utf-8");
-	socket.on("data", (message) => {
-		console.log(`${remoteSocket} -> ${message}`);
+const END = "END";
+
+const connections = new Map();
+
+const error = (message) => {
+	console.error(message);
+	process.exit(1);
+};
+
+const sendMessage = (message, origin) => {
+	// Enviar a todos menos a origin el message
+};
+
+const listen = (port) => {
+	const server = new Server();
+	server.on("connection", (socket) => {
+		const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+		console.log(`New connection from ${remoteSocket}`);
+		socket.setEncoding("utf-8");
+
+		socket.on("data", (message) => {
+			if (!connections.has(socket)) {
+				console.log(`Username ${message} set for connection ${remoteSocket}`);
+				connections.set(socket, message);
+			} else if (message === END) {
+				console.log(`Connection with ${remoteSocket} closed`);
+				socket.end();
+			} else {
+				// Enviar message a los demas clientes
+				console.log(`${remoteSocket} -> ${message}`);
+			}
+		});
+
+		socket.on("error", (err) => error(err.messagemessage));
 	});
-});
 
-server.listen({ port: 8000, host: "0.0.0.0" }, () => {
-	console.log("Listening on port 8000");
-});
+	server.listen({ port, host }, () => {
+		console.log("Listening on port 8000");
+	});
+	server.on("error", (err) => error(err.message));
+};
+
+const main = () => {
+	if (process.argv.length !== 3) {
+		error(`Usage: node ${__filename} port`);
+	}
+
+	let port = process.argv[2];
+	if (isNaN(port)) {
+		error(`Invalid port ${port}`);
+	}
+
+	port = Number(port);
+	listen(port);
+};
+
+if (require.main === module) {
+	main();
+}
